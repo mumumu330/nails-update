24e48ff7bf8304b09f7ea2ff712fa97bc9dd2e1d
mumumu330 <ritz30akn@gmail.com>
2020-11-12 19:00:11 +0900

    add notice method

--- app/models/user.rb
+++ app/models/user.rb
@@ -44,4 +44,14 @@ class User < ApplicationRecord
     end
   end
 
+  def create_notification_follow!(current_user)
+    temp = Notification.where(["visiter_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+    if temp.blank?
+      notification = current_user.active_notifications.new(
+        visited_id: id,
+        action: 'follow'
+      )
+      notification.save if notification.valid?
+    end
+  end
 end
