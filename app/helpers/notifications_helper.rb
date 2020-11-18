adead86ee369686e5ad490ed713e117e2d8e7c80
mumumu330 <ritz30akn@gmail.com>
2020-11-18 11:34:24 +0900

    fix notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,23 +1,21 @@
 module NotificationsHelper
   def notification_form(notification)
-    @visiter = notification.visiter
+    @visiter = notification.where(visiter_id =! current_user.id)
     @comment = nil
-    your_post_image = link_to 'あなたの投稿', users_post_image_path(notification), style:"font-weight: bold;"
+    your_post_image = link_to 'あなたの投稿', post_image_path(notification), style:"font-weight: bold;"
     @visiter_comment = notification.post_comment_id
     #notification.actionがfollowかlikeかcommentか
     case notification.action
       when "follow" then
-        tag.a(notification.visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"があなたをフォローしました"
       when "favorite" then
-        tag.a(notification.visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_post_image_path(notification.post_image_id), style:"font-weight: bold;")+"にいいねしました"
+        tag.a(notification.visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_image_path(notification.post_image_id), style:"font-weight: bold;")+"にいいねしました"
       when "post_comment" then
           @comment = PostComment.find_by(id: @visiter_comment)&.content
-          tag.a(@visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:users_post_image_path(notification.post_image_id), style:"font-weight: bold;")+"にコメントしました"
+          tag.a(@visiter.name, href:user_path(@visiter), style:"font-weight: bold;")+"が"+tag.a('あなたの投稿', href:post_image_path(notification.post_image_id), style:"font-weight: bold;")+"にコメントしました"
     end
   end
-
   def unchecked_notifications
     @notifications = current_user.passive_notifications.where(checked: false)
   end
-  
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -6,4 +6,9 @@ class Notification < ApplicationRecord
     belongs_to :post_comment, optional: true
     belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
     belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+
+  def visiter_name
+    User.find(visiter_id).name
+  end
+
 end
