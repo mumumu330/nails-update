861a645b6cedaf3a697954028734affd5411161f
mumumu330 <ritz30akn@gmail.com>
2020-11-18 11:32:17 +0900

    fix favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,9 +2,10 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user!
   
   def create
+    @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.new(post_image_id: @post_image.id)
     favorite.save
-    @post_image = PostImage.find(params[:post_image_id])
+    
     #通知の作成
     @post_image.create_notification_by(current_user)
     respond_to do |format|
@@ -14,6 +15,7 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    byebug
     @post_image = PostImage.find(params[:post_image_id])
     favorite = current_user.favorites.find_by(post_image_id: @post_image.id)
     favorite.destroy
