fbfe8cbeecd6d952ad27e268f4b5a5c6933f06cb
mumumu330 <ritz30akn@gmail.com>
2020-11-22 04:20:28 +0900

    fix relationships controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def follow
+    @user = User.find(params[:id])
     current_user.follow(params[:id])
+    @user.create_notification_follow!(current_user)
     redirect_to root_path
 
     #通知の作成
-    @user = User.find(params[:follower_id])
-    @user.create_notification_follow!(current_user)
-    redirect_to root_path
+    # @user = User.find(params[:follower_id])
+    # @user.create_notification_follow!(current_user)
+    # redirect_to root_path
   end
   
   def unfollow
